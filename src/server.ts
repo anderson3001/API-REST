50f31076c730c9b2efd5cba9b682224d7f39fb31
Anderson Gomes <derson3001@gmail.com>
2025-05-01 15:00:16 -0300

    corrijindo responses

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,6 @@ app.listen({
     host: "0.0.0.0",
     port: env.PORT
 }).then(() => {
-    console.log(`Server is running at http://localhost:${env.PORT}`)
+    console.log(`Servidor: http://localhost:${env.PORT}`)
+    console.log(`\nDocumentação: http://localhost:${env.PORT}/api-docs`)
 })
